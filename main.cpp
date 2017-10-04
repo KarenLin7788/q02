f42e42883b570a233521a63ed7d052ce0bbada40
karenlin7788 <xlin2@mail.csuchico.edu>
2017-10-04 10:49:39 -0700

    Answer: git push git commit -mAnswer:

--- main.cpp
+++ main.cpp
@@ -24,6 +24,12 @@ int main()
   //the first and second blanks should be replaced with the coordinates
   //of the element's row and column and the third blank should be replaced
   //with the value of that element
+string list [NUM_ROWS][NUM_COLS];
+list[0][0]="a";
+list[0][1]="b";
+list[0][2]="c";
+cout<<"ROW:"<<NUM-ROWS<<"Column:"<<NUM-COLS<<"is"<<list[NUM-ROWS][NUM_COLS]<<end;
+
 
   
 
